f7c9bf048186728a8e4d76eed1ed8bff059f60b7
Jens Thewes <35758939+jthewes@users.noreply.github.com>
2023-08-03 20:26:23 +0200

    feat: separate quote scheduling and notification scheduling

--- src/main/java/de/zedalite/quotes/scheduling/QuoteOfTheDayScheduler.java
+++ src/main/java/de/zedalite/quotes/scheduling/QuoteOfTheDayScheduler.java
@@ -1,6 +1,9 @@
 package de.zedalite.quotes.scheduling;
 
 import de.zedalite.quotes.data.model.PushNotification;
+import de.zedalite.quotes.exceptions.NotifierException;
+import de.zedalite.quotes.exceptions.QuoteNotFoundException;
+import de.zedalite.quotes.exceptions.ResourceNotFoundException;
 import de.zedalite.quotes.repository.PushNotificationRepository;
 import de.zedalite.quotes.service.QuoteService;
 import org.slf4j.Logger;
@@ -9,6 +12,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * The QuoteOfTheDayScheduler class is responsible for scheduling the reset of the quote of the day
@@ -29,8 +34,8 @@ public class QuoteOfTheDayScheduler {
   /**
    * Initializes a new instance of the QuoteOfTheDayScheduler class.
    *
-   * @param quoteService         the QuoteService used to retrieve the quote of the day
-   * @param notifierRepository  the PushNotificationRepository used to send notification
+   * @param quoteService       the QuoteService used to retrieve the quote of the day
+   * @param notifierRepository the PushNotificationRepository used to send notification
    */
   public QuoteOfTheDayScheduler(final QuoteService quoteService, final PushNotificationRepository notifierRepository) {
     this.quoteService = quoteService;
@@ -38,15 +43,38 @@ public class QuoteOfTheDayScheduler {
   }
 
   /**
-   * Resets the quote of the day by emptying the cache, retrieving a new quote from the QuoteService,
-   * and sending a push notification using the PushNotificationRepository.
-   * This method is scheduled to run every day at 8:00 AM.
+   * Clears the cache for the quote of the day and retrieves a new quote from the quote service.
+   * This method is scheduled to run daily.
    */
-  @Scheduled(cron = "0 8 * * * *")
+  @Scheduled(cron = "@daily")
   public void resetQuoteOfTheDay() {
     emptyQotdCache();
     quoteService.findQuoteOfTheDay();
-    sendPushNotification();
+  }
+
+
+  /**
+   * Sends a push notification for the quote of the day.
+   * This method is scheduled to run every day at 12:00 PM.
+   */
+  @Scheduled(cron = "0 0 12 * * *")
+  public void sendPushNotification() {
+    try {
+      final var quoteId = quoteService.findQuoteOfTheDay().id();
+
+      final var notification = new PushNotification(
+        "Quote Of The Day",
+        "Check out the inspiration for today",
+        Map.of("type", "QOTD", "quoteId", String.valueOf(quoteId))
+      );
+
+      notifierRepository.sendToTopic(qotdTopic, notification);
+      LOGGER.info("PushNotification for qotd sent, topic={}, quoteId={}", qotdTopic, quoteId);
+    } catch (final QuoteNotFoundException | ResourceNotFoundException ex) {
+      LOGGER.warn("PushNotification for qotd failed, no or not enough quote(s) found");
+    } catch (final NotifierException ex) {
+      LOGGER.warn("PushNotification for qotd failed, topic={}.", qotdTopic);
+    }
   }
 
   /**
@@ -55,16 +83,5 @@ public class QuoteOfTheDayScheduler {
   @CacheEvict(value = "qotd", allEntries = true)
   public void emptyQotdCache() {
     LOGGER.info("Cache QOTD emptied");
-  }
-
-  /**
-   * Sends a push notification to the specified topic.
-   */
-  private void sendPushNotification() {
-    final var notification = new PushNotification(
-      "Quote Of The Day",
-      "Check out the inspiration for today");
-    notifierRepository.sendToTopic(qotdTopic, notification);
-    LOGGER.info("Push notification sent to topic={}.", qotdTopic);
   }
 }
--- src/test/java/de/zedalite/quotes/scheduling/QuoteOfTheDaySchedulerTest.java
+++ src/test/java/de/zedalite/quotes/scheduling/QuoteOfTheDaySchedulerTest.java
@@ -1,6 +1,8 @@
 package de.zedalite.quotes.scheduling;
 
 import de.zedalite.quotes.data.model.PushNotification;
+import de.zedalite.quotes.exceptions.QuoteNotFoundException;
+import de.zedalite.quotes.fixtures.QuoteGenerator;
 import de.zedalite.quotes.repository.PushNotificationRepository;
 import de.zedalite.quotes.service.QuoteService;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class QuoteOfTheDaySchedulerTest {
@@ -31,6 +33,27 @@ class QuoteOfTheDaySchedulerTest {
     instance.resetQuoteOfTheDay();
 
     then(quoteService).should().findQuoteOfTheDay();
+  }
+
+  @Test
+  @DisplayName("Should send push notification")
+  void shouldSendPushNotification() {
+    final var qotd = QuoteGenerator.getQuote();
+    willReturn(qotd).given(quoteService).findQuoteOfTheDay();
+
+    instance.sendPushNotification();
+
+    then(quoteService).should().findQuoteOfTheDay();
     then(notifierRepository).should().sendToTopic(any(), any(PushNotification.class));
+  }
+
+  @Test
+  @DisplayName("Should not send notification when no qotd found")
+  void shouldNotSendNotificationWhenNoQotdFound() {
+    willThrow(QuoteNotFoundException.class).given(quoteService).findQuoteOfTheDay();
+
+    instance.sendPushNotification();
+
+    then(notifierRepository).shouldHaveNoInteractions();
   }
 }
