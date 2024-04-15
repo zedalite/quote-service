978e948441ba1105dac0190003f1500f01023e06
Jens Thewes <35758939+jthewes@users.noreply.github.com>
2024-04-15 22:08:17 +0200

    fix: enable csrf

--- src/main/java/de/zedalite/quotes/config/SecurityConfiguration.java
+++ src/main/java/de/zedalite/quotes/config/SecurityConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -39,7 +38,6 @@ public class SecurityConfiguration {
   public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
     http
       .cors(c -> c.configurationSource(getCorsConfiguration()))
-      .csrf(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests(authz -> authz.requestMatchers("/actuator/*").permitAll().anyRequest().authenticated())
       .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
       .authenticationProvider(authenticationProvider)
